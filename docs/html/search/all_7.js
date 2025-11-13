75af1a09a0799bed946c79602f23c134909a8902
pschatzmann <phil.schatzmann@gmail.com>
2025-11-13 21:38:31 +0100

    doxygen

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['io_0',['io',['../classfatfs_1_1IO.html',1,'fatfs::IO'],['../group__io.html',1,'IO']]]
+  ['io_0',['io',['../classfatfs_1_1IO.html',1,'fatfs::IO'],['../group__io.html',1,'IO']]],
+  ['iterator_1',['iterator',['../group__iterator.html',1,'']]]
 ];
--- docs/html/search/groups_1.js
+++ docs/html/search/groups_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['io_0',['IO',['../group__io.html',1,'']]]
+  ['io_0',['IO',['../group__io.html',1,'']]],
+  ['iterator_1',['iterator',['../group__iterator.html',1,'']]]
 ];
